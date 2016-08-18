6bca8db64dfe415a7f9640012ab68e5e102d24b7
arnold-okoth <arnoldkyaye@gmail.com>
2016-08-18 17:51:10 +0300

    Super sum solution plus unit tests

--- TDD/super_sum/super_sum.py
+++ TDD/super_sum/super_sum.py
@@ -1,5 +1,15 @@
 def super_sum(*args):
-	if len(args) == 0:
-		return 'list empty'
-	else:
-		if type(args) == list:
+	total = 0
+	total_arg_list = 0
+	total_arg_number = 0
+	for argument in args:
+		if type(argument) == list:
+			for element in argument:
+				total_arg_list = total_arg_list + element
+		else:
+			total_arg_number = total_arg_number + argument
+
+	total = total_arg_number + total_arg_list
+	return total
+
+print(super_sum(1,2,4,[1,2]))
--- TDD/super_sum/test_super_sum.py
+++ TDD/super_sum/test_super_sum.py
@@ -15,16 +15,19 @@ class SuperSumTest(unittest.TestCase):
 
 
 	def test_nested_list(self):
-		self.assertEqual(10, super_sum([1,2,3,[2,2]]))
-		self.assertEqual(15, super_sum([4,5,[4,2]]))
-		self.assertEqual(60, super_sum([10,20,[30]]))
+		self.assertEqual(10, super_sum(1,2,3,[2,2]))
+		self.assertEqual(15, super_sum(4,5,[4,2]))
+		self.assertEqual(60, super_sum(10,20,[10,20]))
 
 	def test_nested_list2(self):
-		self.assertEqual(100, super_sum([50,[20,10,10,10]]))
-		self.assertEqual(200, super_sum([150, [10,30,10]]))
-		self.assertEqual(150, super_sum([75,[75]]))
+		self.assertEqual(100, super_sum(50,20,10,10,10))
+		self.assertEqual(200, super_sum(150,10,[30,10]))
+		self.assertEqual(150, super_sum(75,75))
 
 	def test_nested_and_normal(self):
 		self.assertEqual(10, super_sum(1,2,3,[2],2))
 		self.assertEqual(40, super_sum(1,2,3,4,[10],10,5,5))
-		self.assertEqual(50, super_sum(10,20,[15],[10],5))
+		self.assertEqual(60, super_sum(10,20,[15],[10],5))
+
+if __name__ == '__main__':
+	unittest.main()
