a2b77ee17a12813964c2eb3099976837d760ed08
arnold-okoth <arnoldkyaye@gmail.com>
2016-08-19 17:08:23 +0300

    Changes to type checks in super_sum function and a unit test

--- TDD/super_sum/super_sum.py
+++ TDD/super_sum/super_sum.py
@@ -2,12 +2,19 @@ def super_sum(*args):
 	total = 0
 	total_arg_list = 0
 	total_arg_number = 0
+	string_error_message = 'your list contains a string'
 	for argument in args:
 		if type(argument) == list:
 			for element in argument:
+				if type(element) is not int:
+					return string_error_message
 				total_arg_list = total_arg_list + element
 		else:
+			if type(argument) is not int:
+				return string_error_message
 			total_arg_number = total_arg_number + argument
 
 	total = total_arg_number + total_arg_list
 	return total
+
+print(super_sum(50,20,[10,10,10]))
